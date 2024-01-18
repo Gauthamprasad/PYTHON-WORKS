bc0cf0ad17982a0dcb9994fe1e7c1843f46f946a
Gautham Prasad <gauthamp24@gmail.com>
2024-01-19 01:36:52 +0530

    First Python File

---
+++ Basic.py
@@ -0,0 +1,42 @@
+#Adding two numbers.
+x = 5
+y = 4
+sum = x + y
+print("Answer is ",sum)
+
+#Substract two numbers.
+x = 5
+y = 4
+diff = x - y
+print("Answer is ",diff)
+
+#Multiplication of two numbers.
+x = 5
+y = 4
+mul = x * y
+print("Answer is ",mul)
+
+#Division of two numbers.
+x = 10
+y = 3
+div = x / y
+print("Answer is ",div)
+
+#Division of two numbers when we don't want float values.
+x = 10
+y = 3
+div = x // y                                        #Here we use "//" called integer division or floor division.
+print("Answer is ",div)
+
+#In division when we want only the remaining value.
+x = 5
+y = 4
+remain = x % y                                      # " % " is used.
+print("Remaining is ",remain)
+
+#To find power
+#If we want to find power of 2 raise to 3.
+
+Power = 2**3                                        # "**" is used to find power.
+print("Power is ",Power )
+
